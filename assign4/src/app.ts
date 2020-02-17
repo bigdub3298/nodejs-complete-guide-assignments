c33265adac918fd5995b9f9a6b75a580764af57c
bigdub3298 <wesleyaustin33@gmail.com>
2020-02-16 17:50:39 -0700

    added middleware to handle form sumbission and save username

--- assign4/src/app.ts
+++ assign4/src/app.ts
@@ -8,10 +8,16 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-const users: Array<{username:string}> = [{username: 'worldwidemess'}]; 
+const users: Array<{username:string}> = []; 
 
 app.get('/users', (_, res) => {
   res.render('users', {pageTitle: 'Users', path: '/users', users}); 
+})
+
+app.post('/users', (req, res) => {
+  const {username} = req.body; 
+  users.push({username});
+  res.redirect(301, '/users');
 })
 app.get("/", (_, res) => {
   res.render("index", { pageTitle: "Add User", path:'/'});
